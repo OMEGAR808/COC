3b238ed09a1598d32dd152b3a8e8c7714878de9e
nayhtetlin007 <143700605+OMEGAR808@users.noreply.github.com>
2023-09-23 20:55:07 +0630

    Update FXF.py

--- FXF.py
+++ FXF.py
@@ -20,7 +20,7 @@ os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install ch
 ugen=[]
 for xd in range(10000):
     a='Mozilla/5.0 (Linux; U; Android'
-    b=random.choice(['6','7','8','9','10','11','12']
+    b=random.choice(['6','7','8','9','10','11','12'])
     c='SAMSUNG SM-'
     d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
     e=random.randrange(1, 999)
